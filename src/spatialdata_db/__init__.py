f40b0c28468f641344add06e63da86e297045c92
zimea <lea.zimmermann@online.de>
2025-01-20 17:25:31 +0100

    wip sddb curator

--- src/spatialdata_db/__init__.py
+++ src/spatialdata_db/__init__.py
@@ -3,9 +3,10 @@ from spatialdata_db.parsing import load_10x_metadata
 from django.core.exceptions import ImproperlyConfigured
 from lamin_utils import logger
 try:
-    from spatialdata_db.lamin_spatialdata_validator import SpatialDataValidator
+    from spatialdata_db.lamin_spatialdata_validator_outdated import SpatialDataValidator
+    from spatialdata_db.lamin_spatialdatadb_curator import SpatialDataDBCurator
 except ImproperlyConfigured:
     logger.warning("Importing SpatialDataValidator currently requires being connected to a lamindb instance.")
 
-__all__ = ["load_10x_metadata", "SpatialDataValidator"]
+__all__ = ["load_10x_metadata", "SpatialDataValidator", "SpatialDataDBCurator"]
 __version__ = version("spatialdata-db")
